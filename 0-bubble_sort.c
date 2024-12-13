0868c234ccab4a42e0159d9d19cc4c1103d51e68
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-13 18:39:08 +0100

    fix error betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,7 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * bubble_sort - Sorts an array of integer in ascending order using Bubble Sort
  * @array: Table of integers to sort
  * @size: Table size
  *
@@ -14,13 +14,16 @@ void bubble_sort(int *array, size_t size)
 int index, comparator, tmp;
 	if (array == NULL || size < 2)
 	return;
-			for (index = 0; index < (int)size - 1; index++) {
-				for (comparator = 0; comparator < (int)size - index - 1; comparator++) {
-	if (array[comparator] > array[comparator + 1]) {
+for (index = 0; index < (int)size - 1; index++)
+{
+	for (comparator = 0; comparator < (int)size - index - 1; comparator++)
+{
+		if (array[comparator] > array[comparator + 1])
+{
 			tmp = array[comparator];
 			array[comparator] = array[comparator + 1];
 			array[comparator + 1] = tmp;
-				print_array(array, size); /* Print after each swap */
+			print_array(array, size); /* Print after each swap */
 }
 }
 }
